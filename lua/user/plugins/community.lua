4182b8b0dfdc88ef13918a889e6286108e72bcde
naveenkanuri <83157625+naveenkanuri@users.noreply.github.com>
2024-01-24 22:37:13 -0600

    Update community.lua

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -6,18 +6,27 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.pack.cmake" },
   { import = "astrocommunity.pack.bash" },
+  -- bars-and-lines
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  -- completion
+  { import = "astrocommunity.completion.copilot-lua" },
   -- colorschemes
   { import = "astrocommunity.colorscheme.everforest", enabled = true },
   { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
+  -- editing-support
+  { import = "astrocommunity.editing-support.treesj" },
   -- motions
   { import = "astrocommunity.motion.flash-nvim", enabled = true },
   { import = "astrocommunity.motion.harpoon", enabled = true },
+  { import = "astrocommunity.motion.mini-move", enabled = true },
   -- diagnostics
   { import = "astrocommunity.diagnostics.trouble-nvim", enabled = true },
+  -- debugging
+  { import = "astrocommunity.debugging.persistent-breakpoints-nvim", enabled = true },
+  { import = "astrocommunity.debugging.nvim-dap-virtual-text", enabled = true },
   -- editing
   { import = "astrocommunity.editing-support.rainbow-delimiters-nvim", enabled = true },
   { import = "astrocommunity.editing-support.todo-comments-nvim", enabled = true },
@@ -25,6 +34,8 @@ return {
   { import = "astrocommunity.editing-support.multicursors-nvim", enabled = true },
   -- scrolling
   { import = "astrocommunity.scrolling.mini-animate", enabled = true },
+  -- markdown-preview-nvim
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim", enabled = true },
   -- syntax
   { import = "astrocommunity.syntax.hlargs-nvim", enabled = true },
   -- test
@@ -36,6 +47,8 @@ return {
   { import = "astrocommunity.bars-and-lines.vim-illuminate", enabled = true },
   -- comment
   { import = "astrocommunity.comment.mini-comment", enabled = true },
+  -- note-taking
+  { import = "astrocommunity.note-taking.obsidian-nvim", enabled = true },
   { -- further customize the options set by the community
     "catppuccin",
     opts = {
@@ -53,7 +66,6 @@ return {
       },
     },
   },
-  { import = "astrocommunity.completion.copilot-lua" },
   { -- further customize the options set by the community
     "zbirenbaum/copilot.lua",
     opts = {
@@ -69,12 +81,26 @@ return {
       },
     },
   },
-  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
   {
     "m4xshen/smartcolumn.nvim",
     opts = {
       colorcolumn = 120,
       disabled_filetypes = { "help" },
+    },
+  },
+  {
+    "Wansmer/treesj",
+    keys = { { "<leader>J", "<CMD>TSJToggle<CR>", desc = "Toggle Treesitter Join" } },
+  },
+  {
+    "folke/flash.nvim",
+    keys = {
+      {
+        "<leader>j",
+        mode = { "n", "x", "o" },
+        function() require("flash").jump() end,
+        desc = "Flash",
+      },
     },
   },
 }
