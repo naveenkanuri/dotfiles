cbab371818c1c9fd164e3411d19f729c70603f0e
naveenkanuri <83157625+naveenkanuri@users.noreply.github.com>
2024-03-10 18:41:42 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,19 +1,22 @@
 #!/bin/zsh
 
-echo "updating brew..."
+echo "updating brew...."
 brew update && brew upgrade --greedy
 
-
 IFS=$'\n' casks=($(brew list --cask))
-
 for cask in "${casks[@]}"; do
     brew upgrade --cask "$cask"
 done
-
 echo "cleaning brew..."
 brew cleanup
 
-echo "updating rust..."
+echo "updating powerlevel10k...."
+git -C ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k pull
+
+echo "updating alacritty themes...."
+git -C $HOME/.config/alacritty/themes pull
+
+echo "udpating rust...."
 rustup update
 
-echo "done..."
+echo "done...."
